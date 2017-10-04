02a782e7437da0b3d586960a018e0b925dc9125d
Jon Hong <jonmhong@gmail.com>
2017-10-03 22:17:54 -0700

    prepare for landing page search

--- js/App.jsx
+++ js/App.jsx
@@ -1,6 +1,8 @@
 // @flow
 
 import React from 'react';
+// BrowserRouter: uses HTML5 API history, to keep UI in sync with URL
+// Switch: renders routes exclusively
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
 import Details from './Details';
@@ -19,24 +21,25 @@ const FourOhFour = () => <h1>404</h1>;
 
 // TODO: update state, so you can search from the front page
 const App = () => (
-    <BrowserRouter>
-        <div className='app'>
-            <Switch>
-                {/* This is how you comment */}
-                <Route exact path="/" component={Landing} />
-                <Route path ="/search" component={
-                    props => <Search shows={preload.shows} {...props}  />
-                } />
-                <Route
-                    path="/details/:id"
-                    component={(props: { match: Match }) => {
-                        const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
-                        return <Details show={selectedShow} {...props} />;
-                    }}/>
-                <Route component={FourOhFour} />
-            </Switch>
-        </div>
-    </BrowserRouter>
+  <BrowserRouter>
+    <div className='app'>
+      <Switch>
+        {/* This is how you comment */}
+        <Route exact path="/" component={Landing} />
+        {/* Loads to show lists page with search */}
+        <Route path="/search" component={
+          props => <Search shows={preload.shows} {...props}  />
+        } />
+        <Route
+          path="/details/:id"
+          component={(props: { match: Match }) => {
+            const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+            return <Details show={selectedShow} {...props} />;
+          }} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
--- js/Search.jsx
+++ js/Search.jsx
@@ -5,42 +5,39 @@ import ShowCard from './ShowCard';
 import Header from './Header';
 
 class Search extends React.Component {
-    state = {
-        searchTerm: ''
-    };
-    props: {
-        shows: Array<Show>,
+  state = {
+    searchTerm: ''
+  };
+  props: {
+    shows: Array<Show>,
+  };
 
-    };
+  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
+    this.setState({ searchTerm: event.target.value });
+  };
 
-    handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
-        this.setState({ searchTerm: event.target.value });
-    };
-
-    render() {
-        return (
-            <div className="search">
-                <Header
-                    showSearch
-                    searchTerm={this.state.searchTerm}
-                    handleSearchTermChange={this.handleSearchTermChange}
-                />
-                <div>
-                    {this.props.shows
-                        .filter(show =>
-                            `${show.title} ${show.description}`.toUpperCase().indexOf(
-                                this.state.searchTerm.toUpperCase()
-                            ) >= 0
-                        )
-                        .map(show =>
-                        <ShowCard key={show.imdbID} {...show} />
-                    )}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="search">
+        <Header
+          showSearch
+          searchTerm={this.state.searchTerm}
+          handleSearchTermChange={this.handleSearchTermChange}
+        />
+        <div>
+          {this.props.shows
+            .filter(show =>
+              `${show.title} ${show.description}`.toUpperCase().indexOf(
+                this.state.searchTerm.toUpperCase()
+              ) >= 0
+            )
+            .map(show =>
+            <ShowCard key={show.imdbID} {...show} />
+          )}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Search;
-
-
